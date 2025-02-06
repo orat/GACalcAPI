9e4112d0284e2aa5d6d49c40dd4be7873c203416
Oliver Rettig <Oliver.Rettig@orat.de>
2025-02-06 12:48:46 +0100

    add linear map

--- src/main/java/util/cga/SparseCGAKVector.java
+++ src/main/java/util/cga/SparseCGAKVector.java
@@ -3,7 +3,9 @@ package util.cga;
 import de.orat.math.sparsematrix.SparseDoubleMatrix;
 
 /**
- *
+ * @Deprecated und wenn ich das doch brauchen sollte, dann solle das von
+ * SparseCGAColumnVector erben
+ * 
  * @author Oliver Rettig (Oliver.Rettig@orat.de)
  */
 public class SparseCGAKVector extends SparseDoubleMatrix {
