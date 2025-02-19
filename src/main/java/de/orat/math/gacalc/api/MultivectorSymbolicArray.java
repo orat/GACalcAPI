37cf15369b1ad1da0643e3c5b401f259c31f727f
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-02-19 16:03:48 +0100

    Added ensureSize

--- src/main/java/de/orat/math/gacalc/api/MultivectorSymbolicArray.java
+++ src/main/java/de/orat/math/gacalc/api/MultivectorSymbolicArray.java
@@ -2,6 +2,7 @@ package de.orat.math.gacalc.api;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class MultivectorSymbolicArray extends ArrayList<MultivectorSymbolic> {
 
@@ -11,5 +12,18 @@ public class MultivectorSymbolicArray extends ArrayList<MultivectorSymbolic> {
 
     public MultivectorSymbolicArray(Collection<? extends MultivectorSymbolic> c) {
         super(c);
+    }
+
+    public void ensureSize(int newSize, MultivectorSymbolic nullElement) {
+        final int oldSize = super.size();
+        if (newSize <= oldSize) {
+            return;
+        }
+        // newSize > oldSize
+        // diff > 0
+        final int diff = newSize - oldSize;
+        super.ensureCapacity(newSize);
+        var nullList = Collections.nCopies(diff, nullElement);
+        super.addAll(nullList);
     }
 }
