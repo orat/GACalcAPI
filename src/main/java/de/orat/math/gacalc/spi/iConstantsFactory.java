60a12c6bbd1539e89c9b7806de5f3939369d86bc
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-10-17 16:24:26 +0200

    Improve const factory

--- src/main/java/de/orat/math/gacalc/spi/iConstantsFactory.java
+++ src/main/java/de/orat/math/gacalc/spi/iConstantsFactory.java
@@ -10,83 +10,82 @@ public interface iConstantsFactory<IMV extends iMultivector<IMV>> {
 
     iExprGraphFactory<?, ?, ?> fac();
 
-    IMV newConstant(String name, SparseDoubleMatrix definition);
-
-    IMV newConstant(String name, IMV definition);
-
-    IMV cached(Supplier<IMV> creator);
+    /**
+     * Supposed to be only used by implementer.
+     */
+    IMV cached(String name, Supplier<SparseDoubleMatrix> creator);
 
     IMV getSparseEmptyInstance();
 
     IMV getDenseEmptyInstance();
 
     default IMV getBaseVectorOrigin() {
-        return cached(() -> newConstant("ε₀", fac().createBaseVectorOrigin(1d)));
+        return cached("ε₀", () -> fac().createBaseVectorOrigin(1d));
     }
 
     default IMV getBaseVectorInfinity() {
-        return cached(() -> newConstant("εᵢ", fac().createBaseVectorInfinity(1d)));
+        return cached("εᵢ", () -> fac().createBaseVectorInfinity(1d));
     }
 
     default IMV getBaseVectorX() {
-        return cached(() -> newConstant("ε₁", fac().createBaseVectorX(1d)));
+        return cached("ε₁", () -> fac().createBaseVectorX(1d));
     }
 
     default IMV getBaseVectorY() {
-        return cached(() -> newConstant("ε₂", fac().createBaseVectorY(1d)));
+        return cached("ε₂", () -> fac().createBaseVectorY(1d));
     }
 
     default IMV getBaseVectorZ() {
-        return cached(() -> newConstant("ε₃", fac().createBaseVectorZ(1d)));
+        return cached("ε₃", () -> fac().createBaseVectorZ(1d));
     }
 
     default IMV getEpsilonPlus() {
-        return cached(() -> newConstant("ε₊", fac().createEpsilonPlus()));
+        return cached("ε₊", () -> fac().createEpsilonPlus());
     }
 
     default IMV getEpsilonMinus() {
-        return cached(() -> newConstant("ε₋", fac().createEpsilonMinus()));
+        return cached("ε₋", () -> fac().createEpsilonMinus());
     }
 
     default IMV getPi() {
-        return cached(() -> newConstant("π", fac().createScalar(Math.PI)));
+        return cached("π", () -> fac().createScalar(Math.PI));
     }
 
     default IMV getBaseVectorInfinityDorst() {
-        return cached(() -> newConstant("∞", fac().createBaseVectorInfinityDorst()));
+        return cached("∞", () -> fac().createBaseVectorInfinityDorst());
     }
 
     default IMV getBaseVectorOriginDorst() {
-        return cached(() -> newConstant("o", fac().createBaseVectorOriginDorst()));
+        return cached("o", () -> fac().createBaseVectorOriginDorst());
     }
 
     default IMV getBaseVectorInfinityDoran() {
-        return cached(() -> newConstant("n", fac().createBaseVectorInfinityDoran()));
+        return cached("n", () -> fac().createBaseVectorInfinityDoran());
     }
 
     default IMV getBaseVectorOriginDoran() {
-        return cached(() -> newConstant("ñ", fac().createBaseVectorOriginDoran()));
+        return cached("ñ", () -> fac().createBaseVectorOriginDoran());
     }
 
     default IMV getMinkovskiBiVector() {
-        return cached(() -> newConstant("E₀", fac().createMinkovskiBiVector()));
+        return cached("E₀", () -> fac().createMinkovskiBiVector());
     }
 
     default IMV getEuclideanPseudoscalar() {
-        return cached(() -> newConstant("E₃", fac().createEuclideanPseudoscalar()));
+        return cached("E₃", () -> fac().createEuclideanPseudoscalar());
     }
 
     default IMV getPseudoscalar() {
-        return cached(() -> newConstant("E", fac().createPseudoscalar()));
+        return cached("E", () -> fac().createPseudoscalar());
     }
 
     IMV getInversePseudoscalar();
 
     default IMV one() {
-        return cached(() -> newConstant("1", fac().createScalar(1d)));
+        return cached("1", () -> fac().createScalar(1d));
     }
 
     default IMV half() {
-        return cached(() -> newConstant("0.5", fac().createScalar(0.5d)));
+        return cached("0.5", () -> fac().createScalar(0.5d));
     }
 }
